48525486096ab1565ec299bccdedfae2360508fb
Hristo Kirilov <hristo.kirilov07@gmail.com>
2023-05-24 18:27:06 +0300

    New file

---
+++ task_basic/loops.py
@@ -0,0 +1,5 @@
+number = int(input())
+
+for i in range(number):
+    if i < number:
+        print(i**2)
---
+++ task_basic/print_function.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    text = ""
+    for number in range(1,n + 1):
+       text+=str(number)
+    print(text)
